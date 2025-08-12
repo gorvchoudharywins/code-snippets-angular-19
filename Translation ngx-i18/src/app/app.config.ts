1e0109638ac12481b869259ac8ed74cc86f3e1d6
Gourav Choudhary <gourav.choudhary@ditstek.com>
2025-08-12 16:15:32 +0530

    dev -  app_initialize changes

--- Translation ngx-i18/src/app/app.config.ts
+++ Translation ngx-i18/src/app/app.config.ts
@@ -1,23 +1,16 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  importProvidersFrom,
-  APP_INITIALIZER,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, provideAppInitializer, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { translationInitializerFactory } from './i18n-init';
+import { initializeApp } from './i18n-init';
 
 // Factory function to create the TranslateHttpLoader
 // This function is used to load translation files from the assets/i18n directory
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
+export const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>
+   new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,20 +20,13 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     // Import the TranslateModule with the HttpLoaderFactory
     // importProvidersFrom is used to import the TranslateModule with the HttpLoaderFactory
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      })
-    ),
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      useFactory: translationInitializerFactory,
-      deps: [TranslateService],
-    },
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })),
+    provideAppInitializer(() => initializeApp(inject(TranslateService))()),
   ],
 };
--- Translation ngx-i18/src/app/i18n-init.ts
+++ Translation ngx-i18/src/app/i18n-init.ts
@@ -2,16 +2,12 @@ import { TranslateService } from '@ngx-translate/core';
 
 export const SUPPORTED_LANGS = ['en', 'es', 'fr'];
 
-export function translationInitializerFactory(translate: TranslateService) {
+export function initializeApp(translate: TranslateService) {
   return () => {
     const browserLang: string = translate.getBrowserLang() || '';
     const lang = SUPPORTED_LANGS.includes(browserLang) ? browserLang : 'en';
 
-    return new Promise((resolve) => {
-      translate.use(lang).subscribe({
-        next: () => resolve(true),
-        error: () => resolve(true),
-      });
-    });
+    // translate.setDefaultLang('en');
+    translate.use(lang);
   };
 }

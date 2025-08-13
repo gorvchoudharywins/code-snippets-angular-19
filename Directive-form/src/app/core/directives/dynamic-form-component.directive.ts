271b0419a60b6848e4c426bb77b6613e24005298
Gourav Choudhary <gourav.choudhary@ditstek.com>
2025-08-13 23:46:19 +0530

    dev - add form array

--- Directive-form/src/app/core/directives/dynamic-form-component.directive.ts
+++ Directive-form/src/app/core/directives/dynamic-form-component.directive.ts
@@ -10,7 +10,7 @@ import {
 import { InputFieldComponent } from '../../shared/components/input-field/input-field.component';
 import { RadioButtonComponent } from '../../shared/components/radio-button/radio-button.component';
 import { DropDownComponent } from '../../shared/components/drop-down/drop-down.component';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormArray } from '@angular/forms';
 
 const DYNAMIC_COMPONENT: { [type: string]: Type<any> } = {
   inputText: InputFieldComponent,
@@ -32,8 +32,25 @@ export class DynamicFormComponentDirective {
   ngOnChanges(): void {
     if (this.group && this.config.controlType === 'group') {
       this.handleGroupControl();
+    } else if (this.group && this.config.controlType === 'formArray') {
+      this.handleFormArrayControl();
     } else if (this.group && this.config.controlType === 'control') {
       this.handleSingleControl();
+    }
+  }
+  private handleFormArrayControl() {
+    this.group.setControl(this.config.control, new FormArray([]));
+    const formArray = this.group.get(this.config.control) as FormArray;
+
+    if (formArray.length === 0) {
+      const group = new FormGroup({});
+      this.config.configs.forEach((config: any) => {
+        const componentType = DYNAMIC_COMPONENT[config.type];
+        if (componentType) {
+          this.createAndInitComponent(componentType, group, config);
+        }
+      });
+      formArray.push(group);
     }
   }
 

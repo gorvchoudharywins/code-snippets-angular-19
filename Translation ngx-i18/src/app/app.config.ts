5a889925308be0d57e7f651af9c4f6c1337be092
Gourav Choudhary <gourav.choudhary@ditstek.com>
2025-08-13 10:37:40 +0530

    dev - multiple app_init use

--- Translation ngx-i18/src/app/app.config.ts
+++ Translation ngx-i18/src/app/app.config.ts
@@ -1,7 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, provideAppInitializer, inject } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+  provideAppInitializer,
+  inject,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateService,
+} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
@@ -10,23 +20,23 @@ import { initializeApp } from './i18n-init';
 // Factory function to create the TranslateHttpLoader
 // This function is used to load translation files from the assets/i18n directory
 export const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>
-   new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+  new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // Provide the HttpClient for the TranslateHttpLoader
     provideHttpClient(),
-    // Import the TranslateModule with the HttpLoaderFactory
-    // importProvidersFrom is used to import the TranslateModule with the HttpLoaderFactory
-    importProvidersFrom(TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
-      },
-    })),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
     provideAppInitializer(() => initializeApp(inject(TranslateService))()),
+    // provideAppInitializer(() => test()()),
   ],
 };
